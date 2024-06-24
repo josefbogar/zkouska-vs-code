68bd759a7be82415cf374ac38d2f4b4bdeb8bfbd
Josef Bogar <bogar.j@seznam.cz>
2024-06-24 22:50:12 +0200

    ahoj3

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ let y = 7;
 let soucet = x + y;
 
 // Vypsání výsledku
-console.log(`Ahoj Josefe, vypočítal jsi to dobře. Součet ${x} +${y} je ${soucet}.`);
+console.log(`Ahoj Josef, vypočítal jsi to dobře. Součet ${x} +${y} je ${soucet}.`);
